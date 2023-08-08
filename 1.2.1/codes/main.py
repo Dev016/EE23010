f2e1ea554e1b6b75950cec9ba74bd1c88c29028c
Dev016 <jaindevansh05@gmail.com>
2023-08-08 22:25:55 +0530

    Inserted Figure

--- 1.2.1/codes/main.py
+++ 1.2.1/codes/main.py
@@ -1,4 +1,34 @@
+# #Code by GVV Sharma
+# #December 7, 2019
+# #released under GNU GPL
+# #Drawing a triangle given 3 sides
+
+# import matplotlib.pyplot as plt
+# import matplotlib.image as mpimg
+# image = mpimg.imread('exit-ramp.jpg')
+# plt.imshow(image)
+# plt.show()
+
+import sys                                          #for path to external scripts
+#sys.path.insert(0, '/home/user/txhome/storage/shared/gitlab/res2021/july/conics/codes/CoordGeo')        #path to my scripts
+sys.path.insert(0, '/home/devansh/EE23010/1.2.1/codes/CoordGeo')        #path to my scripts
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+#local imports
+from line.funcs import *
+
+from triangle.funcs import *
+from conics.funcs import circ_gen
+
+
+#sys.path.insert(0, '/home/user/txhome/storage/shared/gitlab/res2021/july/conics/codes/CoordGeo')        #path to my scripts
+
+#if using termux
+import subprocess
+import shlex
+#end if
 
 #The following points are given
 A = np.array([1,-1])
@@ -17,4 +47,44 @@ F = (A + B)/2
 print("D:", list(D))
 print("E:", list(E))
 print("F:", list(F))
+
+#Generating all lines
+x_AB = line_gen(A,B)
+x_BC = line_gen(B,C)
+x_CA = line_gen(C,A)
+
+
+#Plotting all lines
+plt.plot(x_AB[0,:],x_AB[1,:],label='$AB$')
+plt.plot(x_BC[0,:],x_BC[1,:],label='$BC$')
+plt.plot(x_CA[0,:],x_CA[1,:],label='$CA$')
+
+
+#Labeling the coordinates
+tri_coords = np.vstack((A,B,C,D,E,F)).T
+plt.scatter(tri_coords[0,:], tri_coords[1,:])
+vert_labels = ['A','B','C','D','E','F']
+for i, txt in enumerate(vert_labels):
+    plt.annotate(txt, # this is the text
+                 (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='best')
+plt.grid() # minor
+plt.axis('equal')
+
+#if using termux
+#plt.savefig('tri_sss.pdf')
+plt.savefig('/home/devansh/EE23010/1.2.1/figs/figure.png')
+
+
+
+
+
+
+
 
