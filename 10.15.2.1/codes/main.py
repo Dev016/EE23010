ec675f2341c6259de7223b6fe0b79396c4c83d01
Dev016 <jaindevansh05@gmail.com>
2023-09-06 01:29:13 +0530

    Updated Python and Figures in Latex

--- 10.15.2.1/codes/main.py
+++ 10.15.2.1/codes/main.py
@@ -26,25 +26,19 @@ print('')
 
 #Calculating propability theoretically
 print("Theoretical Probability")
-theoretical = {-4: np.array([0.2,0,0,0,0])@(np.array([0.2,0.2,0.2,0.2,0.2])),
-	       -3: np.array([0.2,0.2,0,0,0])@(np.array([0.2,0.2,0.2,0.2,0.2])),
-	       -2: np.array([0.2,0.2,0.2,0,0])@(np.array([0.2,0.2,0.2,0.2,0.2])),
-	       -1: np.array([0.2,0.2,0.2,0.2,0])@(np.array([0.2,0.2,0.2,0.2,0.2])),
-	       0: np.array([0.2,0.2,0.2,0.2,0.2])@(np.array([0.2,0.2,0.2,0.2,0.2])),
-	       1: np.array([0.2,0.2,0.2,0.2,0])@(np.array([0.2,0.2,0.2,0.2,0.2])),
-	       2: np.array([0.2,0.2,0.2,0,0])@(np.array([0.2,0.2,0.2,0.2,0.2])),
-	       3: np.array([0.2,0.2,0,0,0])@(np.array([0.2,0.2,0.2,0.2,0.2])),
-	       4: np.array([0.2,0,0,0,0])@(np.array([0.2,0.2,0.2,0.2,0.2]))}
-
+theoretical = dict(map(lambda k: (k, (5-abs(k))/25), range(-4,5)))
+print(theoretical)
 #Cumulative probability
-cdf_th = {0 : theoretical[0]}
+'''cdf_th = {0 : theoretical[0]}
 cdf_th[1] = cdf_th[0] + theoretical[-1] + theoretical[1]
 cdf_th[2] = cdf_th[1] + theoretical[-2] + theoretical[2]
 cdf_th[3] = cdf_th[2] + theoretical[-3] + theoretical[3]
 cdf_th[4] = cdf_th[3] + theoretical[-4] + theoretical[4]
-
+'''
+cdf_th = dict(map(lambda k : (k,sum(list(map(lambda i : (5-abs(i))/25, range(-k,k+1))))),range(0,5))) 
+print(cdf_th)
 #PMF of |Z|
-pmf_z2 = [cdf_th[0], cdf_th[1]-cdf_th[0],cdf_th[2]-cdf_th[1],cdf_th[3]-cdf_th[2],cdf_th[4]-cdf_th[3]]
+pmf_z2 = list(map(lambda k : (5-abs(k))/25 + np.sign(k)*(5 - abs(k))/25 , range(0,5))) 
 
 print("Same Day:" , pmf_z2[0])
 print("Consecutive Days:" , pmf_z2[1])
@@ -72,7 +66,7 @@ plt.clf()
 
 plt.stem(z1, cdf_th.values(), linefmt='b-', markerfmt='bo')
 plt.stem(z1, cdf_z1, linefmt='none', markerfmt='go')
-plt.xlabel('$Z$')
+plt.xlabel('$|Z|$')
 plt.ylabel('CDF')
 plt.title("CDF of |Z|")
 plt.legend(['Theoretical', 'Simulated'])
