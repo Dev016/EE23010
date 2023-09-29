a0f5b43b14c408317131735bee608a2a0531d779
Dev016 <jaindevansh05@gmail.com>
2023-09-30 01:31:08 +0530

    Updated Gaussian 9.3.10

--- 9.3.10/codes/main.py
+++ 9.3.10/codes/main.py
@@ -3,22 +3,22 @@ import matplotlib.pyplot as plt
 from scipy.stats import binom, norm
 
 n = 4  
-p = 0.90  
+p = 0.10  
 k = np.arange(0, n+1)  
 binomial_pmf = binom.pmf(k, n, p)
 print("Binomial")
-print(binomial_pmf[n])
+print(binomial_pmf[0])
 mu = n * p
 sigma = np.sqrt(n * p * (1 - p))
 x = np.linspace(0, n, 1000)
 normal_pdf = norm.pdf(x, mu, sigma)
 print("Gaussian")
-print(normal_pdf[len(x)-1])
+print(normal_pdf[0])
 plt.stem(k, binomial_pmf, label='Binomial PMF', basefmt='b-')
 plt.plot(x, normal_pdf, label='Normal PDF', color='r')
-plt.axvline(x=9, color='g', linestyle='--', label='x=9')
-plt.xlabel('Number of Balls Drawn which are not marked zero')
-plt.ylabel('Probability')
+plt.xlabel('Number of Balls Drawn which are marked zero')
+plt.ylabel('Probability / Probability Density')
 plt.legend()
-plt.savefig("./figure.png")
+plt.title("Binomial PMF vs Gaussian PDF")
+plt.savefig("/home/devansh/EE23010/9.3.10/figs/figure.png")
 plt.show()
